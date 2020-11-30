8fd6b617bdc5adfa498c11601f6677eae175c348
jake222333 <59941620+jake222333@users.noreply.github.com>
2020-11-30 15:28:08 -0800

    Login Page Test
    updated

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar backgroundColor="#1e90ff" barStyle="light-content" />
+      <Text style={styles.welcome}>Login to My App</Text>
+      <TextInput style={styles.input} placeholder="Username" />
+      <TextInput style={styles.input} placeholder="Password" secureTextEntry />
+      <View style={styles.btnContainer}>
+        <TouchableOpacity
+          style={styles.userBtn}
+          onPress={() => alert("Login Works")}
+        >
+          <Text style={styles.btnTxt}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.userBtn}
+          onPress={() => alert("Login Works")}
+        >
+          <Text style={styles.btnTxt}>Signup</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -14,8 +36,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    backgroundColor: "#1e90ff",
+    alignItems: "center",
+  },
+  welcome: {
+    color: "#fff",
+    marginBottom: 10,
+    textAlign: "center",
+  },
+  input: {
+    width: "90%",
+    backgroundColor: "#fff",
+    padding: 15,
+    marginBottom: 10,
+  },
+  btnContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "90%",
+  },
+  userBtn: {
+    backgroundColor: "#FFD700",
+    padding: 15,
+    width: "45%",
+  },
+  btnTxt: {
+    fontSize: 18,
+    textAlign: "center",
   },
 });
