9d71159975b8494350ceff24aa9f1ff0040cff09
Yuxuan1993 <yuxuan866@gmail.com>
2020-11-30 20:11:12 -0500

    Login screen added

--- App.js
+++ App.js
@@ -1,14 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{Component} from 'react';
+import { 
+  StyleSheet, 
+  Text, 
+  View,
+  TouchableOpacity,
+  TextInput
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Login to my App</Text>
+        <StatusBar style="auto" />
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+        />
+         <TextInput 
+          style={styles.input} 
+          placeholder="Password" 
+          secureTextEntry 
+        />
+        <View style={styles.btnContainer}>
+          <TouchableOpacity
+            style={styles.userBtn}
+            onPress={() => alert("Login Works")}
+          >
+            <Text style={styles.btnTxt}>Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.userBtn}
+            onPress={() => alert("Login Works")}
+          >
+            <Text style={styles.btnTxt}>Signup</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -17,5 +48,32 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor:'#1e90ff'
+  },
+  welcome:{
+    fontSize: 30,
+    textAlign: 'center',
+    margin:10,
+    color:"#fff"
+  },
+  input:{
+    width:"90%",
+    backgroundColor:"#fff",
+    padding:15,
+    marginBottom:10
+  },
+  btnContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "90%",
+  },
+  userBtn: {
+    backgroundColor: "#FFD700",
+    padding: 15,
+    width: "45%",
+  },
+  btnTxt: {
+    fontSize: 18,
+    textAlign: "center",
   },
 });
