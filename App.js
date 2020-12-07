16466d0e870af1e819cbd02b2194061444a18deb
Yuxuan1993 <yuxuan866@gmail.com>
2020-12-07 17:34:59 -0500

    Revised  login and onboard screens

--- App.js
+++ App.js
@@ -1,70 +1,21 @@
-import { StatusBar } from "expo-status-bar";
-import React,{Component} from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  TouchableOpacity,
-} from "react-native";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default class App extends Component {
-  render(){
-      return(
-      <View style={styles.container}>
-        <StatusBar backgroundColor="#1e90ff" barStyle="light-content" />
-        <Text style={styles.welcome}>Login to My App</Text>
-        <TextInput style={styles.input} placeholder="Username" />
-        <TextInput style={styles.input} placeholder="Password" secureTextEntry />
-        <View style={styles.btnContainer}>
-          <TouchableOpacity
-            style={styles.userBtn}
-            onPress={() => alert("Login Works")}
-          >
-            <Text style={styles.btnTxt}>Login</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.userBtn}
-            onPress={() => alert("Login Works")}
-          >
-            <Text style={styles.btnTxt}>Signup</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    );
-  }
-}
+import OnboradingScreen from "./screens/OnboardingScreen";
+import LoginScreen from "./screens/LoginScreen";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    backgroundColor: "#1e90ff",
-    alignItems: "center",
-  },
-  welcome: {
-    color: "#fff",
-    marginBottom: 10,
-    textAlign: "center",
-  },
-  input: {
-    width: "90%",
-    backgroundColor: "#fff",
-    padding: 15,
-    marginBottom: 10,
-  },
-  btnContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    width: "90%",
-  },
-  userBtn: {
-    backgroundColor: "#FFD700",
-    padding: 15,
-    width: "45%",
-  },
-  btnTxt: {
-    fontSize: 18,
-    textAlign: "center",
-  },
-});
+const AppStack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <AppStack.Navigator headerMode="none">
+        <AppStack.Screen name="Onborading" component={OnboradingScreen} />
+        <AppStack.Screen name="Login" component={LoginScreen} />
+      </AppStack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
